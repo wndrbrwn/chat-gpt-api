96b99a8cad6a76e57666255d843991fd1f5b2c2f
wndrbrwn <jyh7678@hotmail.com>
2024-05-16 17:53:09 +0900

    sss

--- src/components/ChatlistCard.jsx
+++ src/components/ChatlistCard.jsx
@@ -1,6 +1,6 @@
 const ChatlistCard = ({ question, answer }) => {
   return (
-    <li className="bg-pink-50 p-4 rounded-md text-lg shadow-md shadow-pink-50">
+    <li className="bg-blue-50 p-4 rounded-md text-lg shadow-md shadow-blue-50">
       <div className="mb-2 font-semibold">Q. {question}</div>
       <div>A. {answer}</div>
     </li>
