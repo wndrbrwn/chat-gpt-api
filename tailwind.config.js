c5f4b2962380b19cc9132b66b5b0a44018e45a31
wndrbrwn <jyh7678@hotmail.com>
2024-05-16 16:53:24 +0900

    gpt

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+      },
     },
   },
   plugins: [],
