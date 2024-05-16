5edf0af5703032dbe2e20ab7e0049da0d11c317d
wndrbrwn <jyh7678@hotmail.com>
2024-05-16 16:40:46 +0900

    vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,7 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      animation: {
-        "spin-slow": "spin 3s linear infinite",
-      },
+      
     },
   },
   plugins: [],
